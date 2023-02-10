0209a02f5ef7e237d9be63cac3fa93055503d3f3
박헌민 <a31950@gmail.com>
2023-02-10 11:36:36 +0900

    start 변수와 함수

--- app/src/main/java/com/example/test/Test.kt
+++
@@ -1,5 +0,0 @@
-package com.example.test
-
-fun main(){
-    println("안녕하세요! 헌민입니다.")
-}
---
+++ app/src/main/java/com/example/test/ch3/Test.kt
@@ -0,0 +1,14 @@
+package com.example.test.ch3
+
+import com.example.test.formatDate
+import com.example.test.User
+import com.example.test.data
+import java.util.*
+
+fun main(){
+   data = 20
+    println("data : $data")
+    var saydate = formatDate(Date())
+    println(saydate)
+    User().sayHello()
+}
